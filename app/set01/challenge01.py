b65682e4b1ac392cda530f19280c48cac431d50b
Sébastien Geay <sebastien@cryptosense.com>
2023-06-02 23:07:50 +0200

    Set 01 Challenge 01

---
+++ app/set01/challenge01.py
@@ -0,0 +1,7 @@
+import base64
+
+
+def challenge01() -> str:
+    string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    raw = bytes.fromhex(string)
+    return base64.b64encode(raw).decode()
---
+++ rust/set01/challenge01.rs
@@ -0,0 +1,11 @@
+use pyo3::prelude::*;
+use hex;
+use base64::{Engine as _, engine::general_purpose};
+
+#[pyfunction]
+pub fn challenge01() -> PyResult<String> {
+    let string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let bytes = hex::decode(string).unwrap();
+    let base64 = general_purpose::STANDARD.encode(bytes);
+    Ok(base64)
+}
