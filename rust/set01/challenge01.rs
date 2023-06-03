0258f175d6bb0d7e2a880988ce4ff97a2de6f052
Sébastien Geay <sebastien@cryptosense.com>
2023-06-03 12:08:29 +0200

    Format

--- rust/set01/challenge01.rs
+++ rust/set01/challenge01.rs
@@ -2,12 +2,14 @@ use pyo3::prelude::*;
 use hex;
 use base64::{Engine as _, engine::general_purpose};
 
+
 #[pyfunction]
 pub fn challenge01(s: String) -> PyResult<String> {
     let bytes = hex::decode(s).unwrap();
     let base64 = general_purpose::STANDARD.encode(bytes);
     Ok(base64)
 }
+
 
 #[pyfunction]
 pub fn challenge01_golf(s: String) -> PyResult<String> {
