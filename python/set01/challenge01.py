3faf254c9961b71d926a52164f62dc921182b77c
Sébastien Geay <sebastien@cryptosense.com>
2023-06-03 11:39:35 +0200

    Format

--- python/set01/challenge01.py
+++ python/set01/challenge01.py
@@ -3,7 +3,8 @@ import base64
 
 def challenge01(s: str) -> str:
     raw = bytes.fromhex(s)
-    return base64.b64encode(raw).decode()
+    b64 = base64.b64encode(raw)
+    return b64.decode()
 
 
 def challenge01_golf(s: str) -> str:
